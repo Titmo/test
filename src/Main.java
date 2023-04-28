0d16226938865e037593b80b8a231a214e17445c
Danila <03denchik03@gmail.com>
2023-04-28 14:35:37 +0300

    gjvjubnt

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        //yjyjyjyj
-        System.out.println("Hello world!");
-        //werwewewewe[eqxyu
+        String[] str = {"aa", "bb", "cc", "aa"};
+        String string="";
+        Set<String> set = new HashSet<String>(Arrays.asList(str));
+        String[] result = set.toArray(new String[0]);
+        for (int i = 0; i < result.length; i++) {
+            string= string + result[i]+" ";
+        }
+        System.out.println(string);
     }
+
 }
